4265ad9ef5594d65dcdf71fe883fbb1093c64e93
Evin Revello <Evin@EvinRevello.com>
2016-05-19 23:10:08 -0700

    structuring and documenting

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # node-page-manager
-This is a way to dynamically control a set of pages with embedded configurations files.
+This is a way to dynamically control a set of pages with embedded configurations files. 
+
+## Usage
+This app relies on a folder/file structure that is similar to what is found in the Apps folder.
+
+Apps: Contains all of the applications/pages that need to be managed
+
+Apps/*: These are the individual app files.
+
+Config File: Contains the human readable name, the explanation, and the link to the actual file ( apps/application_1/index.html ).
+
+Apps
+   Application 1
+       Config File
+       HTML Resource
+   Application 3
+       Config File
+       HTML Resource
+   Application 3
+       Config File
+       HTML Resource
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var cheerio = require('cheerio');
 var expressapp     = express();
 var bodyParser = require('body-parser');
 var exec = require('child_process').exec;
+var dive = require('dive');
 
 //Configuring middleware
 expressapp.use(bodyParser())
@@ -13,87 +14,20 @@ expressapp.use(bodyParser())
 expressapp.use(express.static('public'));
 
 //Maps the assets request to the resources/js file
-expressapp.use('/assets', express.static(__dirname + '/public/resources'));
-
-//Demo of Cheerio
-expressapp.get('/basicGet', function(req, res){
-    console.log('Get made...');
-    var $ = cheerio.load('<h1> Congratulaitons you won!');
-    res.send( $.html());
-})
-
-//Demo of Request to post a form
-expressapp.post('/doPost', function(req, res){
-    console.log(req);
-    console.log('name: ' + req.param('username'));
-
-    res.send('The post was successful: ' + req.param('username'));
-})
+expressapp.use('/mock', express.static(__dirname + '/mock-responses'));
 
 
-//How to use the command line from the web
-expressapp.get('/commandLine', function (req, res) {
-    exec('curl https://www.google.com',
-        function (error, stdout, stderr) {
-        console.log('stdout: ' + stdout);
-        res.send( stdout);
-    });
-})
-
-
-//Dynamically string replacement on a file, rather than creating a new page
-//the name and config can be replaced via the proxy request and query string 
-//parameters.
-expressapp.get('/replaceFile', function(req, res) {
-    var file = fs.readFileSync('filepath','UTF-8');
-
-    file = file.replace('https://www.google.com/', req.headers.host);
-    file = file.replace('http')
-
-
-    // these are equivalent to res.send()
-    res.write(file);
-    res.end();
-})
 
 
 //Dynamic file system management
 expressapp.get('/list', function(req, res) { //returns a list of app pages
-    //lists the apps via the folder structure
-    var list;
-    var getApps = function () { 
-        return new Promise(function(resolve, reject) { 
-            
-            exec('for i in $(ls ./public/apps); do echo ${i%%/}; done',
-                function (error, stdout, stderr) {
-                    list = stdout.split('\n');
-                    list.pop() // removes the last element, which is empty
-                    
-                    console.log(list);
-                    // return list;
-                    
-                    // res.send(list);
-            });
-            resolve(list);
-        });
-    }
+ 
+dive(process.cwd() + '/apps/', { recursive: false, directories: true, files: false }, function(err, dir) {
+  if (err) throw err;
+  console.log(dir);
+});
 
-    Promise.all([getApps(), getApps()])
-    .catch(function(err){ return console.error('Error in Promises.all()',err); })
-    .then(function(msg) {
-        // ^^ 'msg' param is an array of returned values from within the resolve above
-        console.log(msg); // array of resolve(msg) from above..
-        res.send(JSON.stringify(msg))
-        
-    });
-
-
-    //lists the config values for each app
-
-    //take object with apps, names, and keys
-
-    //render the UI dynamically
-    var $ = cheerio.load('<h1> Congratulaitons you won!');
+    var $ = cheerio.load('<h1> Here is your list');
     res.send( $.html());
 })
 
